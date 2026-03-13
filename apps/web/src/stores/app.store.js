5bbff171a6e3125980ff8954fffbaf259f0c272d
Leonardo <leoferolive@gmail.com>
2026-03-12 23:21:19 -0300

    chore(web): impedir emissao de artefatos ts no frontend

--- apps/web/src/stores/app.store.js
+++
@@ -1,7 +0,0 @@
-import { create } from 'zustand';
-export const useAppStore = create((set) => ({
-    clicks: 0,
-    increment: () => {
-        set((state) => ({ clicks: state.clicks + 1 }));
-    },
-}));
